5a143742958041ef780eb1a6f8b28ca4e5555131
laya <layap52@gmail.com>
2023-01-24 16:23:08 -0500

    made changes

--- Task/Task1/file1.py
+++ Task/Task1/file1.py
@@ -1,12 +1,12 @@
-##1. Create three variables in a single line and assign values to them in such a manner that each one of
-#them belongs to a different data type.
+'''1. Create three variables in a single line and assign values to them in such a manner that each one of
+#them belongs to a different data type.'''
 
 a,b,c = 1,2.01,'string'
 print(a)
 print(b)
 print(c)
 
-##2. Create a variable of type complex and swap it with another variable of type integer.
+'''2. Create a variable of type complex and swap it with another variable of type integer.'''
 
 complex = 2+3j        #complex number
 num = 1               #integer
@@ -14,8 +14,8 @@ complex,num = num,complex
 print(num)
 print(complex)
 
-##3. Swap two numbers using a third variable and do the same task without using any third variable.
-#1st method
+'''3. Swap two numbers using a third variable and do the same task without using any third variable.
+#1st method '''
 n1 = 2
 n2 = 3
 print('before swapping:', n1)
@@ -35,11 +35,20 @@ n1,n2 = n2,n1
 print('after swapping:',n1)
 print('after swapping:',n2)
 
+'''4. Write a program that takes input from the user and prints it using both Python 2.x and Python 3.x
+Version.'''
+name = input("Enter the name ")
+print(name)                         ## python3
 
-#5. Write a program to complete the task given below:
-#Ask users to enter any 2 numbers in between 1-10 , add the two numbers and keep the sum in
-#another variable called z. Add 30 to z and store the output in variable result and print result as the
-#final output.
+name = input(["Enter the name "])
+print(name)                         ##python 2
+
+
+
+'''5. Write a program to complete the task given below:
+Ask users to enter any 2 numbers in between 1-10 , add the two numbers and keep the sum in
+another variable called z. Add 30 to z and store the output in variable result and print result as the
+final output.'''
 
 n1 = int(input("Enter the first number b/w 1 and 10: "))
 n2 = int(input("Enter the second number b/w 1 and 10: "))
@@ -47,7 +56,7 @@ z = (n1+n2)
 result = z+30
 print(result)
 
-#6. Write a program to check the data type of the entered values.
+'''6. Write a program to check the data type of the entered values.'''
 
 n1 = 3                          ## integer type
 n2 = 3.45                       ## float
@@ -66,15 +75,15 @@ print(type(n6))
 print(type(n7))
 print(type(n8))
 
-##7.Create Variables using formats such as Upper CamelCase, Lower CamelCase, SnakeCase and
-#UPPERCASE. 
+'''7.Create Variables using formats such as Upper CamelCase, Lower CamelCase, SnakeCase and
+UPPERCASE.''' 
 
-my_var = 15           ## snake_case
-MyVar = 10            ## UpperCamel
-myVar = 12            ## lowerCamel
-MYVAR = 14            ## UPPERCASE
+c = hello_world       ## snake_case
+c = HelloWorld        ## UpperCamel
+c = helloWorld        ## lowerCamel
+c = HELLOWORLD       ## UPPERCASE
 
-##8.8. If one data type value is assigned to ‘a’ variable and then a different data type value is assigned to ‘a’
-#again. Will it change the value? If Yes then Why?
+'''8.8. If one data type value is assigned to ‘a’ variable and then a different data type value is assigned to ‘a’
+again. Will it change the value? If Yes then Why?'''
 
 answer-yes it will change the value because python is dynamically typed programming language
--- Task/Task1/file2.py
+++ Task/Task1/file2.py
@@ -1,4 +1,4 @@
-##1. Write a program in Python to perform the following operation:
+'''1. Write a program in Python to perform the following operation:'''
 
 number = int(input("Enter the number "))
 if number%3 == 0 and number%5 ==0:
@@ -8,9 +8,38 @@ elif number % 5 == 0:
 else:
       print("consultadd")
 
+'''2. Write a program in Python to perform the following operator based task:'''     
+
+n1 = int(input("enter number1 "))
+n2 = int(input("enter number2 "))
+print("Enter 1 - Addition")
+print("Enter 2 - subtraction")
+print("Enter 3 - division")
+print("Enter 4 - multiplication")
+print("Enter 5 - average")
+
+choice = int(input("enter the number"))
+
+if choice == 1:
+    addition = n1+n2
+    print("addition of two numbers is", addition)
+elif choice == 2 :
+    subtraction = n1-n2
+    print("subtraction of two numbers is", subtraction)
+elif choice == 3:
+    division= n1/n2
+    print("division of two numbers is", division)
+elif choice == 4:
+    multiplication = n1*n2
+    print("multiplication of two numbers is", multiplication)
+elif choice == 5:
+    avg = (n1+n2)/2
+    print("avg of two numbers is", avg)
+else:
+    print("NEGATIVE")
 
 
-##3. Write a program in Python to implement the given flowchart:
+'''3. Write a program in Python to implement the given flowchart:'''
 
 a=10
 b=20
@@ -33,7 +62,7 @@ else:
     print("avg is just higher than c")
 
 
-##4. Write a program in Python to break and continue if the following cases occurs:
+'''4. Write a program in Python to break and continue if the following cases occurs:'''
 
 while True:
     num = int(input("Enter the number: "))
@@ -44,14 +73,16 @@ while True:
         print("keep going")
         continue
 
-##5. Write a program in Python which will find all such numbers which are divisible by 7 but are not a
-##multiple of 5, between 2000 and 3200.
+'''5. Write a program in Python which will find all such numbers which are divisible by 7 but are not a
+multiple of 5, between 2000 and 3200.'''
 
 for i in range(2000, 3200):
     if (i%7==0) and (i%5!=0):
         print(i)
+    else:
+        print("entered number is not divisible by 7 and multiple of 5")
 
-##6. What is the output of the following code examples?
+'''6. What is the output of the following code examples?'''
 
 x = 123
 for i in x:     
@@ -72,7 +103,7 @@ while i < 5:
 #error
 #2
 
-##7. Write a program that prints all the numbers from 0 to 6 except 3 and 6.
+'''7. Write a program that prints all the numbers from 0 to 6 except 3 and 6.'''
 
 for i in range(6):
     if i == 3 or i==6:
@@ -80,8 +111,8 @@ for i in range(6):
     print(i, end = ' ')
 print("\n")
 
-##8. Write a program that accepts a string as an input from the user and calculate the number of digits
-##and letters.
+'''8. Write a program that accepts a string as an input from the user and calculate the number of digits
+and letters.'''
 
 string = "Hello 123"
 count1 = 0
@@ -92,4 +123,49 @@ for i in string:
     count2 += 1
 print("no.of digits ", count1)
 print("no.of letters ", count2)
+
+'''9. Read the two parts of the question below:
+Write a program such that it asks users to “guess the lucky number”. If the correct number is
+guessed the program stops, otherwise it continues forever.'''
+
+
+import random
+rn = random.randint(1,10)
+while True:
+    gn = int(input('Guess a number between 1 and 10 '))
+    if rn==gn:
+        print("well guessed!")
+        break
+    else:
+        print("incorrect")
     
+'''10. Write a program that asks five times to guess the lucky number. Use a while loop and a counter,
+such as'''
+
+counter = 1
+while counter <= 5:
+   number = input("Type in the " + str(counter) + " number ")
+   if number != 5:
+       print("Try again.")
+   else:
+       print("Good guess!")
+   counter = counter +1
+else:
+   print("Game over")
+
+'''11. In the previous question, insert break after the “Good guess!” print statement. break will terminate
+the while loop so that users do not have to continue guessing after they found the number. If the user
+does not guess the number at all, print “Sorry but that was not very successful”.'''
+
+
+counter = 1
+while counter <= 5:
+   number = input("Type in the " + str(counter) + " number ")
+   if number != 5:
+       print("Try again.")
+   else:
+       print("Good guess!")
+       break
+   counter = counter +1 
+else:
+   print("Sorry but that was not very successful.")
